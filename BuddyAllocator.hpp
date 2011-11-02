e2ff0e1c3567ad1212770f01ccc75b773fa83948
Jeremy Wright <jeremy@codestrokes.com>
2011-11-01 22:40:25 -0700

    Working

--- BuddyAllocator.hpp
+++ BuddyAllocator.hpp
@@ -12,35 +12,34 @@
 #include <cstddef>
 #include <list>
 #include <cmath>
+#include <cassert>
 #include <boost/integer/static_log2.hpp>
 #include <queue>
-
+#include <pthread.h>
+#include <algorithm>
 using std::queue;
 using std::list;
-
+using std::min;
+using std::find;
 /**
  * @warning This class is may not be used as a base class.
  */
-template <typename T, size_t numBlocks=200>
+template <typename Block, size_t numBlocks=200>
 class BuddyAllocator {
+    friend class TestBuddyAllocator;
     public:
         // The enum is just a way to get the size value at compile time instead
         // of at runtime. 
         enum compileConstants {
-            blockSize=sizeof(T),
+            blockSize=sizeof(Block),
             freeListOrder = ((boost::static_log2<numBlocks>::value))
         };
          
-        struct Block {
-            uint8_t padding[blockSize];
-        };
-
         typedef Block* BlockPtr;
         typedef Block const * BlockConstPtr;
             
-        BuddyAllocator() throw()
+        BuddyAllocator()
         {
-            memoryPool = new Block[numBlocks];
             freeList[freeListOrder].freeBlocks.push_back(memoryPool);
             for(size_t i = 0; i < freeListOrder+1; ++i)
             {
@@ -48,6 +47,38 @@ class BuddyAllocator {
             } 
         }
 
+      
+        ~BuddyAllocator()
+        {
+        }
+
+        // Return the maximum allocatable size
+        size_t max_size() const throw()
+        {
+            return sizeof(sizeof(Block)*numBlocks);
+        }
+
+        //Allocate but do not initialize num elements of type T
+        BlockPtr allocate(size_t num, BlockConstPtr hint = 0)
+        {
+            BlockPtr p = 0;
+            allocateBlock(p, size_to_level(num));
+            return p;
+        }
+
+       //deallocate storage p of deleted items
+       void deallocate(BlockPtr p, size_t num)
+       {
+           releaseBlock(p, size_to_level(num));
+       }
+
+    private:
+       //Make the Buddy Allocator uncopyable
+        template <typename U>
+            BuddyAllocator(const BuddyAllocator<U, numBlocks>&) throw()
+            {
+            }
+ 
         BuddyAllocator(BuddyAllocator const &) throw()
         {
         }
@@ -66,130 +97,118 @@ class BuddyAllocator {
             return size;
         }
 
-        template <typename U>
-            BuddyAllocator(const BuddyAllocator<U, numBlocks>&) throw()
+        Block memoryPool[numBlocks];
+
+        struct MemoryRequest {
+            MemoryRequest()
             {
+                pthread_mutex_init(&wait, NULL);
+                pthread_mutex_lock(&wait); //Lock so the process will sleep immediately
+                request = NULL;
             }
-
-        ~BuddyAllocator()
-        {
-            delete [] memoryPool;
-        }
-
-        // Return the maximum allocatable size
-        size_t max_size() const throw()
-        {
-            return sizeof(sizeof(Block)*numBlocks);
-        }
-
-        //Allocate but do not initialize num elements of type T
-        BlockPtr allocate(size_t num, BlockConstPtr hint = 0)
-        {
-            BlockPtr p = 0;
-            allocateBlock(p, size_to_level(num));
-            return p;
-        }
-
-        //Initialize elements of allocated storage p with value 
-        void construct(BlockPtr p, const T& value)
-        {
-        }
-
-        //destroy elements f  initialized storage p
-        void destroy(BlockPtr p)
-        {
-            p->~T();
-        }
-
-       //deallocate storage p of deleted items
-       void deallocate(BlockPtr p, size_t num)
-       {
-           releaseBlock(p, size_to_level(num));
-       }
-
-    private:
-        BlockPtr memoryPool;
+            pthread_mutex_t wait;
+            BlockPtr request;
+        };
       
         struct freeListHead {
             size_t Nrequested;
-            queue<BlockPtr> waitingRequests;
+            queue<MemoryRequest> waitingRequests;
             list<BlockPtr> freeBlocks;
             pthread_mutex_t lock;
         };
 
         freeListHead freeList[freeListOrder+1];
 
-        inline bool buddyIsFree(BlockPtr M, BlockPtr& Buddy, size_t level)
+        inline bool buddyIsFree(BlockPtr& M, BlockPtr& Buddy, size_t level)
         {
-            return true;
+            ///TODO: Is there a cleaner way to do this, and avoid the casts?
+            //Static_cast, doesn't work
+            BlockPtr BuddySA = (BlockPtr)((size_t)M ^ level_to_size(level));
+            if(find(freeList[level].freeBlocks.begin(), 
+                        freeList[level].freeBlocks.end(), BuddySA) 
+                    != freeList[level].freeBlocks.end())
+            {
+                Buddy = BuddySA;
+                return true;
+            }
+            return false;
+        }
+
+        void lock(size_t level)
+        {
+            pthread_mutex_lock(&freeList[level].lock);
+        }
+
+        void unlock(size_t level)
+        {
+            pthread_mutex_unlock(&freeList[level].lock);
         }
 
         void allocateBlock(BlockPtr& p, size_t level)
         {
             assert(level <= freeListOrder);
             bool doSplit = false;
-            //lock freelist[i]
+            lock(level);
             if(freeList[level].freeBlocks.empty())
             {
                 //No blocks, so wait until some are available
-                freeList[level].waitingRequests.push(p);
+                MemoryRequest selfPending;
+                freeList[level].waitingRequests.push(selfPending);
                 if(freeList[level].waitingRequests.size() > freeList[level].Nrequested)
                 {
                     doSplit = true;
                     freeList[level].Nrequested += 2;
                 }
-                //unlock freelist i
+                unlock(level);
                 if(doSplit)
                 {
                     splitBlock(level + 1);
-                    //sleep
+                  //  pthread_mutex_lock(&selfPending.wait);
                 }
             }
             else
             {
-                //Get a block from the head of the free list
-                //unlock freeList[level]
+                p = freeList[level].freeBlocks.front();
+                freeList[level].freeBlocks.pop_front();
+                unlock(level);
             }
         }
 
         void releaseBlock(BlockPtr M, size_t level)
         {
-            //lock freelist level
-            if(freeList[level].waitingRequests > 0)
+            lock(level);
+            if(freeList[level].waitingRequests.size() > 0)
             {
                 //Give to the blocked request if possible
-                BlockPtr P = freeList[level].waitingRequests.front();
-                freeList[level].waitingRequests.pop_front();
-                //unlock freelist level
-                //wakeup P and give it M
+                MemoryRequest P = freeList[level].waitingRequests.front();
+                freeList[level].waitingRequests.pop();
+                unlock(level);
+                P.request = M;
+                pthread_mutex_unlock(&P.wait);
             }
             else
             {
                 BlockPtr buddy = 0;
                 if(buddyIsFree(M, buddy, level))
                 {
-                    //remove buddy from free list
-                    //unlock freelist[i]
-                    //combine M and buddy
+                    //Coalesce
+                    freeList[level].freeBlocks.remove(buddy);
+                    unlock(level);
+                    combine(M, buddy);
                     releaseBlock(min(M,buddy), level+1);
                 }
                 else
                 {
                     //Don't combine
-                    //add M to the free list
-                    //unlock freelist[i]
+                    freeList[level].freeBlocks.push_back(M);
+                    unlock(level);
                 }
             }
         }
 
-        void release(size_t level, BlockPtr p)
-        {
-
-        }
-
         BlockPtr combine(BlockPtr A, BlockPtr B)
         {
-
+            return A;
         }
 
         void splitBlock(size_t level)
@@ -198,31 +217,33 @@ class BuddyAllocator {
             allocateBlock(p, level);
             BlockPtr M = p;
             BlockPtr B = p+(level_to_size(level)/2);
-            //lock free list-1
+            lock(level-1);
+            MemoryRequest remembered;
             freeList[level-1].Nrequested -= 2;
             /* Satisfying the request for 2 */
             if(freeList[level-1].waitingRequests.size() > 0)
             {
-                BlockPtr p = freeList[level-1].waitingRequests.front();
+                remembered = freeList[level-1].waitingRequests.front();
                 freeList[level-1].waitingRequests.pop();
-                //Wake up P and give it M
+                remembered.request = M;
+                
                 if(freeList[level-1].waitingRequests.size() >0)
                 {
-                    BlockPtr q = freeList[level-1].waitingRequests.front();
+                    remembered = freeList[level-1].waitingRequests.front();
                     freeList[level-1].waitingRequests.pop();
-                    //Wake up Q and give it B
+                    remembered.request = B;
                 }
                 else
                 {
                     freeList[level-1].freeBlocks.push_back(B); //Add B to the free list
                 }
-                //unlock free list-1
-                //Wake up remembered process
+                unlock(level-1);
+                pthread_mutex_unlock(&remembered.wait);
             }
             else
             {
-                //unlock free list-1
-                release(level, combine(M,B));
+                unlock(level-1);
+                releaseBlock(combine(M,B), level);
             }
 
         }
--- Tests/TestBuddyAllocator.cpp
+++ Tests/TestBuddyAllocator.cpp
@@ -3,21 +3,67 @@
 
 #include <list>
 using std::list;
+class TestBuddyAllocator : public ::testing::Test {
+    protected:
+        virtual void SetUp()
+        {
+        }
 
-TEST(MultiplyTest, NormalTest)
+        virtual void TearDown()
+        {
+        }
+
+        BuddyAllocator<int> myAlloc;
+        BuddyAllocator<int, 1> shortAlloc;
+        virtual void invariant()
+        {
+             //The freeBlocks have a maximum number of elements
+            for(int i = 0; i < myAlloc.freeListOrder; ++i)
+            {
+                EXPECT_LE(myAlloc.freeList[i].freeBlocks.size(), i);
+            }
+        
+        }
+};
+
+TEST_F(TestBuddyAllocator, SingleItem)
 {
+
+    BuddyAllocator<int, 1>::BlockPtr NulBlockPtr = NULL;
+    BuddyAllocator<int, 1>::BlockPtr i = myAlloc.allocate(1);
+    ASSERT_NE(i, NulBlockPtr);
+
+    *i = 1;
+    ASSERT_EQ(*i, 1);
+}
+
+TEST_F(TestBuddyAllocator, InitialFreeBlocks)
+{
+    //Initially there should only be 1 free block
+}
+
+TEST_F(TestBuddyAllocator, NormalTest)
+{
+    invariant();
     EXPECT_EQ(1, 1);
 }
 
-TEST(AllocatorInterface, VectorMultipleItem)
+TEST_F(TestBuddyAllocator, VectorMultipleItem)
 {
-    BuddyAllocator<int> myAlloc;
+    BuddyAllocator<int>::BlockPtr NulBlockPtr = NULL;
+    invariant();
     BuddyAllocator<int>::BlockPtr handles[10];
     for(int i=0; i < 10; i++)
+    {
         handles[i] = myAlloc.allocate(1);
+        ASSERT_NE(handles[i], NulBlockPtr);
+        *(handles[i]) = i;
+    }
 
     for(int i = 0; i < 10; i++)
-        EXPECT_NE(1, 0);
+    {
+        EXPECT_EQ(*(handles[i]), i);
+    }
 }
 /*
 TEST(AllocatorInterface, Vector1Item)
