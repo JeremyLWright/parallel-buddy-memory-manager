2c2d33c906f46a627cdbc356af2e19de5b619363
Jeremy Wright <jeremy@codestrokes.com>
2011-11-06 17:17:38 -0700

    Updated the run script.

--- runscript.sh
+++ runscript.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#PBS -l nodes=1:ppn=16
+#PBS -l nodes=1:ppn=8
 #PBS -l walltime=00:05:00
 #PBS -l pmem=20mb
 #PBS -m abe
 #PBS -M jeremy.wright@asu.edu
 
 cd $PBS_O_WORKDIR
-time ./build/buddyDriver
+time ./build/buddyDriver 32
